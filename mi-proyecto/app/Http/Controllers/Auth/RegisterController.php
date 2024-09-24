fc7bdfcae244d9e82d3aacd65e0a0945cbee41b8
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-09-23 18:06:18 -0600

    .

---
+++ mi-proyecto/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
+
+class RegisterController extends Controller
+{
+    use RegistersUsers;
+
+    protected $redirecTo = '/home';
+
+    public function __construct()
+
+    {
+        $this->middleware("guest");
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [ 
+            'name' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string','max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'max:20'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+}
