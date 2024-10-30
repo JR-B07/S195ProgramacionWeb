4f69361d4426715734332a669208c8a53b14ca65
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-10-29 23:05:15 -0600

    Creación de Index
    Creación de la vista principal del proyecto usando el "extends" para usar el codigo de la platilla.

---
+++ biblioteca/resources/views/Index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Principal')
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Bienvenido a la Biblioteca</h1>
+        <p>Descubre noticias literarias y más</p>
+    </div>
+    <p class="text-center">Última noticia literaria: ¡Nuevo libro disponible para registro!</p>
+@endsection
