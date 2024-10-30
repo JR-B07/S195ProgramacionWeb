a046efea0725735463c7bccaf913ca4ce5b0edab
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-10-29 23:28:14 -0600

    Configuración del lenguaje de los mensajes de alerta
    Agregamos las formas en que pueden aparecer los mensajes de alerta siendo en este caso ingles y español

---
+++ biblioteca/resources/lang/en/validate.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => 'The :attribute field is required.',
+    'numeric' => 'The :attribute field must be a number.',
+    'digits' => 'The :attribute field must be exactly :digits digits.',
+    'email' => 'The :attribute field must be a valid email address.',
+    'max' => [
+        'string' => 'The :attribute field must not exceed :max characters.',
+    ],
+    'min' => [
+        'numeric' => 'The :attribute field must be at least :min.',
+        'string' => 'The :attribute field must have at least :min characters.',
+    ],
+];
---
+++ biblioteca/resources/lang/es/validate.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'numeric' => 'El campo :attribute debe ser un número.',
+    'digits' => 'El campo :attribute debe tener exactamente :digits dígitos.',
+    'email' => 'El campo :attribute debe ser una dirección de correo válida.',
+    'max' => [
+        'string' => 'El campo :attribute no debe tener más de :max caracteres.',
+    ],
+    'min' => [
+        'numeric' => 'El campo :attribute debe ser al menos :min.',
+        'string' => 'El campo :attribute debe tener al menos :min caracteres.',
+    ],
+];
