04cea5e9b9d2071b5def4aa57df8a25b128ca71b
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-10-29 23:19:24 -0600

    Creacion de clases en controlador
    Creamos las clases que nos permitirán movernos entre las vistas.
    Uno de las clases la usaremos para validar los datos de los libros, asi como también le agregamos un redireccionamiento al registro y un mensaje de confirmacion

--- biblioteca/app/Http/Controllers/BibliotecaController.php
+++ biblioteca/app/Http/Controllers/BibliotecaController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class BibliotecaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('principal');
+    }
+
+    public function registro()
+    {
+        return view('registro');
+    }
+
+    public function guardarLibro(Request $request)
+    {
+        // Validaciones
+        $request->validate([
+            'isbn' => 'required|numeric|digits:13',
+            'titulo' => 'required|string|max:150',
+            'autor' => 'required|string',
+            'paginas' => 'required|integer|min:1',
+            'anio' => 'required|integer|min:1000|max:' . date('Y'),
+            'editorial' => 'required|string',
+            'email_editorial' => 'required|email'
+        ]);
+
+        // Mensaje de confirmación
+        return redirect()->route('registro')->with('message', 'Todo correcto: Libro “' . $request->titulo . '” guardado');
+    }
 }
