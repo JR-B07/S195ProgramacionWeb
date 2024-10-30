399f8b45d33e6f942a51655cbad11368c80faba1
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-10-29 23:20:32 -0600

    Creación de Rutas
    Creamos las rutas con las clases del controlador

--- biblioteca/routes/web.php
+++ biblioteca/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BibliotecaController;
 
 Route::get('/', function () {
     return view('Index');
 });
+
+Route::get('/', [BibliotecaController::class, 'Index'])->name('Index');
+Route::get('/registro', [BibliotecaController::class, 'registro'])->name('registro');
+Route::post('/registro', [BibliotecaController::class, 'guardarLibro'])->name('guardarLibro');
